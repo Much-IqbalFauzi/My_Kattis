279a0a51b5ca883c5d3059dc3302f0dafde094b6
zipeco <fauziqbal2@gmail.com>
2020-02-03 16:40:04 +0700

    kattis solution

---
+++ encodedmessage.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    __ = input()
+    ___ = int(len(__)**0.5)
+    ____ = [__[_____:_____+___] for _____ in range(0, len(__), +___)]
+    for _____ in range(1, ___+1):
+        for ______ in range(___):
+            print(____[______][-_____], end='')
+    print()
