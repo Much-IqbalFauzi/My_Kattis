165df79c6f3dd15447cd438847df79d7133ebe49
Muchamad Iqbal Fauzi <iqbalfauzi.much@gmail.com>
2023-11-20 08:36:10 +0700

    kattis solution

---
+++ ameriskur.kt
@@ -0,0 +1,10 @@
+
+
+fun main() {
+
+  val ffield = 0.09144
+
+  val input = readLine()!!.toDouble()
+
+  print(ffield * input)
+}
---
+++ fifa.kt
@@ -0,0 +1,12 @@
+
+fun main() {
+
+  val current = 2022
+
+  val num1 = readLine()!!.toInt()
+  val num2 = readLine()!!.toInt()
+
+
+  print(num1 / num2 + current)
+
+}
