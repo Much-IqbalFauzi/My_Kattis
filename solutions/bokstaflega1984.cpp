b6043d4e4141131bc832c3ee3db2a1c9286af016
Muchamad Iqbal Fauzi <iqbalfauzi.much@gmail.com>
2026-07-18 14:03:46 +0700

    solution

---
+++ solutions/bokstaflega1984.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    if (n == 1984) {
+        cout << "Literally 1984" << endl;
+    } else {
+        cout << "Not 1984... yet" << endl;
+    }
+}
